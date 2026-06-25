64a049f5836bc0948eff5a83e7fc5d535061a854
jjuandd <118311783+jjuandd@users.noreply.github.com>
2026-06-25 14:41:03 -0500

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/JuanDiego.py.py
@@ -0,0 +1,37 @@
+
+#https://python.org
+
+#una sola linea
+
+"""esto también
+    es un comentario"""
+
+'''
+ Esto tambien es para
+ escribir python
+'''
+""" python no tiene constante"""
+
+my_variable = "Hola mundo"
+my_variable = "nuevo valor de la variable"
+
+MY_CONSTANT = "Mi constante" #por convencion
+
+ENTERO = 5
+FLOTANTES= -2.5
+my_bolll= True
+my_bolll= False
+CADENA_TEXTO= "hOLA MUNDO"
+my_other_string = 'Hola otro mundo'
+
+print(type(ENTERO))
+print(type(FLOTANTES))
+print(type(my_bolll))
+print(type(my_bolll))
+print(type(CADENA_TEXTO))
+print(type(my_other_string))
+
+
+print("Hola, mi lenguaje es python")
+ 
+ 
